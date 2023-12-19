790d7690a024e6ad27030685a7bc89360ddfee2f
Walid Mohamed <walidridge1@gmail.com>
2023-12-19 08:56:57 +0300

    update on bot

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import telebot
 # from telebot.async_telebot import AsyncTeleBot
 from dotenv import load_dotenv
 from datetime import datetime
+import requests
 load_dotenv()
 
 BOT_TOKEN  = os.environ.get('BOT_TOKEN')
+BASE_URL  = os.environ.get('BASE_URL')
+
+
 bot = telebot.TeleBot(BOT_TOKEN)
 # bot = AsyncTeleBot(BOT_TOKEN)
 print("bot-tokn", BOT_TOKEN)
@@ -38,19 +42,66 @@ DATASET =  {
 Location = ["loc1", "loc2", "loc3", "loc4", "loc5",  "loc6"]
 UNITS = ["u1", "u2", "u3", "u4", "u5",  "u6"]
 COMMODITIES = ["c1", "c2", "c3", "c4", "c5",  "c6"]
-
+messages = []
 @bot.message_handler(commands=['start'])
 def start_command(message):
     # Create a markup keyboard with two options
+    #  -------- main code here
 
-    markup = telebot.types.InlineKeyboardMarkup()
-    option1 = telebot.types.InlineKeyboardButton('Today', callback_data='Today')
-    option2 = telebot.types.InlineKeyboardButton('Other', callback_data='Other')
-    markup.add(option1, option2)
-    
+    # markup = telebot.types.InlineKeyboardMarkup()
+    # option1 = telebot.types.InlineKeyboardButton('Today', callback_data='Today')
+    # option2 = telebot.types.InlineKeyboardButton('Other', callback_data='Other')
+
+    # markup.add(option1, option2)
+    # ---- main code here up
     # Send a message with the markup keyboard
+    # -- main code here
 
-    bot.send_message(message.chat.id, 'Please choose an option:', reply_markup=markup)
+    # bot.send_message(message.chat.id, 'Please choose an option:', reply_markup=markup)
+    
+    #  main code here up
+    
+    USER_ID =  message.from_user.id
+    # res = requests.get(BASE_URL + f'message/{USER_ID}')
+    # res = requests.post(BASE_URL + f'message/{USER_ID}', {
+    #     'message': messages,
+    # })
+    # print('testing response in telegram bot', res.json())
+
+    # userMessages = res.json()
+    
+    # bot.send_message(message.chat.id, 'list of retrieved user messages: ')
+    # print('user messages', userMessages['userM'][0])
+    # ----------
+    # arr = userMessages['userM']
+    # arr_str = '\n'.join(map(str, arr))
+    # bot.send_message(message.chat.id, f'list of retrieved user messages:\n{arr_str}')
+    #  -------
+    bot.send_message(message.chat.id, 'Hello, welcome to my bot!')
+    bot.send_message(message.chat.id, 'start entering messages here and when you are done enter .(period) to stop recording')
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text.endswith('.'):
+        print('messages after period', messages)
+        bot.send_message(message.chat.id, 'Stopping message recording.')
+        USER_ID =  message.from_user.id
+        data = {
+            "message": messages
+        }
+        res = requests.post(BASE_URL + f'message/{USER_ID}', json=data)
+        print('res val', res)
+        
+        # bot.send_message(message.chat.id, f'response sent successfully: {res.json()}')
+        bot.send_message(message.chat.id, 'messsage(s) sent sucessfully')
+        res2 = requests.get(BASE_URL + f'message/{USER_ID}')
+        print('res2 val', res2)
+        bot.send_message(message.chat.id, f'backend response is {res2.json()}')
+        # print('res from backend', res.json())
+    else:
+        messages.append(message.text)
+    # print('messages', messages)
+
     
     # keyboard = [[telebot.types.InlineKeyboardButton(str(i), callback_data=str(i)) for i in range(2)], telebot.types.InlineKeyboardButton('next', callback_data='next')]
     # reply_markup = telebot.types.InlineKeyboardMarkup(keyboard)
